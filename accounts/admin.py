54d63708c1004bccbfa59daca0b8ea0afb2c163c
reza <yadegarireza50@googlemail.com>
2025-02-26 19:39:04 +0330

    socials app started

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-
+from django.utils.translation import gettext_lazy as _ 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
