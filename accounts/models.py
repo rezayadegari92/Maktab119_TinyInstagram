ecc66b258b5b0003370fde4092589548b9507c90
reza <yadegarireza50@googlemail.com>
2025-02-27 14:26:28 +0330

    models for signals developed and for accounts modified

--- accounts/models.py
+++ accounts/models.py
@@ -3,17 +3,34 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager
 
+
+from django.core.exceptions import ValidationError
+
+def validate_file_size(value):
+    filesize = value.size
+    max_size = 5 * 1024 * 1024 
+    if filesize > max_size :
+        raise ValidationError("max size for upload is 5MB")
+
 class CustomUser(AbstractUser):
-    username = None  # Remove the username field
+    #add username 
+    #indexing for username search it 
+    username = models.CharField(
+        _("username"),
+        unique=True,
+        db_index=True,
+        help_text=_("Required. 150 characters or fewer. letters, digits and @/./+/-/_ only."),
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
     email = models.EmailField(_("email address"), unique=True)
     first_name = models.CharField(_("first name"), max_length=20)
     last_name = models.CharField(_("last name"), max_length=20)
     birth_date = models.DateField(_("birth date"))
     phone_number = models.CharField(_("phone number"), max_length=15, blank=True, null=True)
-    profile_picture = models.ImageField(_("profile picture"), upload_to='profile_pictures/', blank=True, null=True)
-
-    USERNAME_FIELD = "email"  # Use email as the unique identifier
-    REQUIRED_FIELDS = ["first_name", "last_name", "birth_date"]  # Add required fields
+    
+    REQUIRED_FIELDS = ["username","first_name", "last_name", "birth_date"]  # Add required fields
 
     objects = CustomUserManager()
 
@@ -41,9 +58,19 @@ class TimestampMixin(models.Model):
         abstract = True  # Ensures this mixin is not created as a separate database table        
 
 
-class Profile(TimestampMixin, models.Model):
+class Profile(TimestampMixin):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, null=False, blank=False, related_name="profile")
     website = models.CharField(max_length=255, blank=True, null=True)
+    #limited image must be added for validation 
+    profile_picture = models.ImageField(_("profile picture"),
+                                         upload_to='profile_pictures/', 
+                                         blank=True, 
+                                         null=True,
+                                         validators=[validate_file_size]
+                                         )
+    #limited image must be added for validation 
+    
+    USERNAME_FIELD = "email"  # Use email as the unique identifier
     bio = models.TextField(blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
     is_private = models.BooleanField(default=False)
@@ -60,14 +87,15 @@ class Profile(TimestampMixin, models.Model):
 from django.utils import timezone
 
 
+
 class Otp(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="otp")
+    email = models.EmailField(_("email_address"))
     otp_code = models.CharField(max_length=6, null=False, blank=False)  # OTP codes are typically 4-6 digits
     created_at = models.DateTimeField(auto_now_add=True)
     expires_at = models.DateTimeField()  # Add an expiration time for the OTP
 
     def __str__(self):
-        return f"OTP for {self.user.email}"
+        return f"OTP for {self.email}"
 
     def is_expired(self):
         """Check if the OTP has expired."""
--- socials/models.py
+++ socials/models.py
@@ -1,3 +1,75 @@
 from django.db import models
-
+from accounts.models import TimestampMixin, CustomUser
 # Create your models here.
+
+class Image(TimestampMixin):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to='posts/')
+    IMAGE_TYPE_CHOICES = (
+        ('post','Post'),
+        ('story','Story'),
+        ('other','Other'),
+    )
+    image_type = models.CharField(max_length=20, choices=IMAGE_TYPE_CHOICES, default='post')
+
+    def __str__(self):
+        return f"image is for {self.user.username}"
+
+class Like():
+    like_status = models.BooleanField(default=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name='likes')
+    post = models.ForeignKey('Post', on_delete=models.CASCADE,related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        unique_together = ('user', 'post')
+
+    def __str__(self):
+        return f"liked by {self.user.username} on post {self.post.id}"    
+
+class Tag:
+    tag_name =  models.TextField(max_length=50, unique=True)
+    def __str__(self):
+        return self.tag_name
+
+class Comment(TimestampMixin):
+    _user = models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name='comments')
+    text = models.TextField()
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return f"comment by {self._user.username} on post {self.post.id}"
+
+
+class Follow:
+    owner =models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name='following')
+    followed_users = models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name='follower')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('owner', 'followed_users')
+    def __str__(self):
+        return f":)"
+    @staticmethod
+    def get_followers(user):
+        return user.follower.all()
+    @staticmethod
+    def get_followings(user):
+        return user.following.all()
+
+class Post(TimestampMixin):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
+    Image = models.ForeignKey(Image,on_delete=models.CASCADE, null=True, blank=True, related_name='posts')
+
+    caption = models.TextField(blank=True, null=True)
+    is_archive = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, on_delete=models.CASCADE, blank=True, related_name='posts') 
+    location = models.CharField(max_length=255, blank=True, null=True)
+
+
+    def __str__(self):
+        return f"post {self.id} by {self.user.username}"
+
+
+
+
+
