72eb8182e606adb89d397f07e5b8d86722036084
reza <yadegarireza50@googlemail.com>
2025-03-11 13:14:32 +0330

    some changes applied

--- core/urls.py
+++ core/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import urls
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -25,7 +26,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.api.v1.urls')),
     path('',include('accounts.urls')),
-    path('socials/', include('socials.api.v1.urls'))
+    path('socials/', include('socials.api.v1.urls')),
+    path('',include('socials.urls')),
 ]
 if settings.DEBUG :
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
